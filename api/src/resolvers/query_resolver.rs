3d753f069f3adbfce42d47948b439b66544a50e7
Alexander Binaei <bigtimetapin@gmail.com>
2025-11-13 14:00:41 -0500

    code tlc

--- api/src/resolvers/query_resolver.rs
+++ api/src/resolvers/query_resolver.rs
@@ -397,7 +397,7 @@ pub async fn preferred_withdraw_validator_list_cacheable_wrapper(
     resolver: Extension<QueryResolver>,
     req: PreferredWithdrawRequest,
 ) -> (StatusCode, Json<Vec<PreferredWithdraw>>) {
-    // Requst defaults
+    // Request defaults
     let min_stake_threshold_default: u64 = 10_000;
     let limit_default: u32 = 50;
 

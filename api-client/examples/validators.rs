da4c808d63748643f2046ea617bb021458723ba2
aoikurokawa <ht.so.ik0102@gmail.com>
2025-11-14 11:42:58 +0900

    feat: add kobe api client library

---
+++ api-client/examples/validators.rs
@@ -0,0 +1,22 @@
+use std::time::Duration;
+
+use kobe_api_client::client_builder::KobeApiClientBuilder;
+
+#[tokio::main]
+async fn main() {
+    let client = KobeApiClientBuilder::new()
+        .timeout(Duration::from_secs(45))
+        .retry(true)
+        .max_retries(5)
+        .build();
+
+    let current_epoch = client.get_current_epoch().await.unwrap();
+    println!("Current epoch: {}\n", current_epoch);
+
+    let validators_res = client.get_validators(Some(current_epoch)).await.unwrap();
+
+    println!("Found {} validators", validators_res.validators.len());
+
+    let first_validator = validators_res.validators[0].clone();
+    println!("Is running bam: {:?}", first_validator.running_bam);
+}
